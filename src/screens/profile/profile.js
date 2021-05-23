5988b7cef476aadc7c98103dc9ff2b1db07fe43a
saranshgupta1995 <saranshgupta1995@gmail.com>
2021-05-23 17:00:00 +0530

    have the profile section

--- src/screens/profile/profile.js
+++ src/screens/profile/profile.js
@@ -1,10 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { withRouter } from "react-router-dom";
 import Header from "../../common/Header";
 import "./profile.css";
 import { IconButton } from "@material-ui/core";
+import { get } from "../../api";
 
 const ProfileScreen = withRouter(({ history }) => {
+  const [userData, setUserData] = useState(null);
+  const [userPicture, setUserPicture] = useState(null);
+  const [userPostCount, setUserPostCount] = useState(null);
+  const [userUsersFollowed, setUserUsersFollowed] = useState(null);
+  const [userUsersFollowedBy, setUserUsersFollowedBy] = useState(null);
+  const [userUsersFullName, setUserUsersFullName] = useState(null);
+  useEffect(() => {
+    get.ids().then(data => {
+      console.log(data);
+      setUserData(data);
+      setUserPicture(data.userImage);
+      setUserPostCount(data.countPosts);
+      setUserUsersFollowed(data.usersFollowed);
+      setUserUsersFollowedBy(data.usersFollowedBy);
+      setUserUsersFullName(data.fullName);
+    });
+  }, []);
   return (
     <>
       <Header
@@ -19,7 +37,22 @@ const ProfileScreen = withRouter(({ history }) => {
           </div>
         }
       ></Header>
-      <main className="home-main"></main>
+      {userData && (
+        <main className="profile-main">
+          <div className="information-section">
+            <img src={userPicture} alt="" />
+            <div className="text-info">
+              <h2>{userUsersFullName}</h2>
+              <div>
+                <span>Posts: {userPostCount}</span>
+                <span>Follows: {userUsersFollowed}</span>
+                <span>Followed By: {userUsersFollowedBy}</span>
+              </div>
+              <p>{userUsersFullName}</p>
+            </div>
+          </div>
+        </main>
+      )}
     </>
   );
 });
