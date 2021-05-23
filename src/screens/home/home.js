33de753587ce739a44262156bb27414ffd2d97c8
saranshgupta1995 <saranshgupta1995@gmail.com>
2021-05-23 10:57:57 +0530

    introduce the card

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -1,17 +1,44 @@
+import {
+  Card,
+  IconButton,
+  Input,
+  CardHeader,
+  CardContent,
+  CardMedia,
+  Typography
+} from "@material-ui/core";
 import React, { useEffect, useState } from "react";
 import Header from "../../common/Header";
 import SearchIcon from "@material-ui/icons/Search";
 import "./home.css";
-import { Input, IconButton } from "@material-ui/core";
 
 const HomeScreen = () => {
-  const [data, setData] = useState([]);
   const [showMenu, setShowMenu] = useState(false);
+  const [instagramPosts, setInstagramPosts] = useState([]);
+
+  const addPost = post => {
+    setInstagramPosts(instagramPosts => [...instagramPosts, post]);
+  };
+
+  const fetchPostData = id => {
+    fetch(
+      "https://graph.instagram.com/17895695668004550?fields=id,media_type,media_url,username,timestamp&access_token=YourAccessToken"
+    ).then(() => {
+      addPost({
+        id: "17895695668004550" + Math.random(),
+        media_type: "IMAGE",
+        media_url: "https://fb-s-b-a.akamaihd.net/...",
+        username: "jayposiris",
+        timestamp: "2017-08-31T18:10:00+0000"
+      });
+    });
+  };
+
   useEffect(() => {
     fetch(
       "https://graph.instagram.com/me/media?fields=id,caption&access_token=8661035776.d0fcd39.39f63ab2f88d4f9c92b0862729ee2784"
     ).then(x => {
-      setData([
+      [
         {
           id: "17895695668004550",
           caption: ""
@@ -28,7 +55,9 @@ const HomeScreen = () => {
           id: "17881042411086627",
           caption: ""
         }
-      ]);
+      ].forEach(d => {
+        fetchPostData(d.id);
+      });
     });
   }, []);
   return (
@@ -60,6 +89,28 @@ const HomeScreen = () => {
           </div>
         }
       ></Header>
+      <main className="post-container">
+        {instagramPosts.map(post => (
+          <Card key={post.id} className="card">
+            <CardHeader
+              avatar={<img src={post.media_url} alt="" />}
+              title={<p className="user-name">{post.username}</p>}
+              subheader={post.timestamp}
+            ></CardHeader>
+            <CardContent>
+              <CardMedia image={post.media_url} title="Paella dish" />
+              <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto
+                  labore rem earum reprehenderit accusantium sequi debitis
+                  aperiam maiores veniam. Magnam inventore eos quasi sit
+                  voluptatum, accusantium nisi doloremque voluptas eaque.
+                </Typography>
+              </CardContent>
+            </CardContent>
+          </Card>
+        ))}
+      </main>
     </>
   );
 };
